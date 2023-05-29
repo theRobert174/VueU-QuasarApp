62450dfe0171f3903ad45440eb806f72b66f1c50
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-05-29 16:26:11 -0600

    col n row system

--- src/pages/Flex.vue
+++ src/pages/Flex.vue
@@ -1,6 +1,27 @@
 <template>
-  <q-page class="flex">
-    <h1>Flex.vue</h1>
+  <q-page>
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col bg-secondary"></div>
+      <div class="col bg-accent"></div>
+    </div>
+    <div class="row q-mb-md">
+      <div class="col bg-primary"></div>
+      <div class="col col-5 bg-secondary"></div>
+      <div class="col col-6 bg-accent"></div>
+    </div>
+    <div class="row q-mb-md justify-center">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+    <div class="row q-mb-md justify-end">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
+    <div class="row q-mb-md justify-between">
+      <div class="col col-4 bg-primary"></div>
+      <div class="col col-4 bg-secondary"></div>
+    </div>
   </q-page>
 </template>
 
@@ -11,3 +32,8 @@ export default defineComponent({
   name: "FlexPage",
 });
 </script>
+<style scoped>
+.col, .col-4, .col-6, .col-12{
+  height: 50px;
+}
+</style>
