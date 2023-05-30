0dd96ea430c81915f1405de9d7a330f4a8eaa14a
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-05-29 18:13:13 -0600

    flexbox quasar

--- src/pages/Flex.vue
+++ src/pages/Flex.vue
@@ -29,6 +29,57 @@
       <div class="col-12 col-sm-6 col-md-4 bg-primary"></div>
       <div class="col-12 col-sm-6 col-md-4 bg-secondary"></div>
     </div>
+
+    <q-separator spaced/>
+
+    <div class="row q-mb-md">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-between">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-end">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="row q-mb-md justify-around">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column q-mb-md items-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column q-mb-md items-end bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-center bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
+    <div class="column custom-height q-mb-md items-center justify-between bg-dark">
+      <div class="square-box bg-primary"></div>
+      <div class="square-box self-start bg-secondary"></div>
+      <div class="square-box bg-accent"></div>
+    </div>
+
   </q-page>
 </template>
 
@@ -42,5 +93,12 @@ export default defineComponent({
 <style scoped>
 .col, .col-4, .col-6, .col-12{
   height: 50px;
+}
+.square-box {
+  height: 50px;
+  width: 50px;
+}
+.custom-height{
+  height: 300px;
 }
 </style>
